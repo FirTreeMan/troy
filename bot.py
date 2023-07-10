513d84b572e19e17d5c4afe64463185605ac8537
FirTreeMan <shilohk.np@gmail.com>
2023-07-10 13:16:23 +0530

    Initial commit

---
+++ bot.py
@@ -0,0 +1,66 @@
+import os
+import discord
+import pygame
+from dotenv import load_dotenv
+from discord.ext import commands
+from pygame import Surface, display, image, Rect, font, FONT_LEFT, FONT_RIGHT, FONT_CENTER
+
+font.init()
+display.init()
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+FONT = "Futura Condensed Extra Bold.otf"
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="troy please ", intents=intents)
+
+
+def drawtext(surface: Surface, text, rect: Rect,
+             color=(0, 0, 0), fontname=FONT, fontsize=80, aa=True, bkg=(255, 255, 255), align=FONT_LEFT):
+    usedfont = font.Font(fontname, fontsize)
+    usedfont.align = align
+
+    while True:
+        fontrect = usedfont.render(text, antialias=aa, color=color, bgcolor=bkg, wraplength=rect.width)
+        if fontrect.get_height() <= rect.height:
+            break
+        fontsize -= 1
+        usedfont = font.Font(fontname, fontsize)
+        usedfont.align = align
+
+    surface.blit(fontrect, rect.topleft)
+
+    return fontrect
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} on')
+
+
+@bot.command(name='live', help='checks for vitals')
+async def live(ctx):
+    await ctx.channel.send("iphone venezuela bottom texxt 100 billion dead")
+
+
+@bot.command(name='make', help='prints a fresh troy')
+async def make(ctx, verb: str = "spit yo shit", obj: str = "Troy", subj: str = "Troy"):
+    imgsurf = image.load('cleantroy.png')
+    surf = Surface(imgsurf.get_size(), pygame.SRCALPHA)
+    surf.blit(imgsurf, (0, 0))
+    verbrect = Rect(36, -10, 336, 93)
+    objrect = Rect(385, -10, 140, 93)
+    subjrect = Rect(252, 85, 140, 93)
+
+    drawtext(surf, verb, verbrect, align=FONT_CENTER)
+    drawtext(surf, obj, objrect, align=FONT_LEFT)
+    drawtext(surf, subj, subjrect, align=FONT_RIGHT)
+    image.save(surf, 'make.png')
+
+    await ctx.channel.send(file=discord.File('make.png'))
+
+
+bot.run(TOKEN)
